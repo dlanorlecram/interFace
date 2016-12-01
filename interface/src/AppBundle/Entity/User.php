f766a982cc1a12ecf1c5411d19f3fc9369c32605
solenehuault <mopi@dotslashplay.it>
2016-12-01 14:07:05 +0100

    Ajout des attributs name et surname dans la classe User.php

--- interface/src/AppBundle/Entity/User.php
+++ interface/src/AppBundle/Entity/User.php
@@ -5,6 +5,7 @@ namespace AppBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
@@ -18,4 +19,46 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+    
+    /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @Assert\NotBlank(message="Please enter your name.", groups={"Registration", "Profile"})
+     * @Assert\Length(
+     *     min=1,
+     *     max=255,
+     *     minMessage="The name is too short.",
+     *     maxMessage="The name is too long.",
+     *     groups={"Registration", "Profile"}
+     * )
+     */
+    protected $name;
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function getName() {
+        return $this->name;
+    }
+    
+    /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @Assert\NotBlank(message="Please enter your surname.", groups={"Registration", "Profile"})
+     * @Assert\Length(
+     *     min=1,
+     *     max=255,
+     *     minMessage="The name is too short.",
+     *     maxMessage="The name is too long.",
+     *     groups={"Registration", "Profile"}
+     * )
+     */
+    protected $surname;
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function getSurname() {
+        return $this->surname;
+    }
 }
