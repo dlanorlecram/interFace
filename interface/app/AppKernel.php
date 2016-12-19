fa94dca8a677c6d542728e594391807a3c13a6fe
solenehuault <mopi@dotslashplay.it>
2016-12-19 17:16:39 +0100

    app/AppKernel.php - Ajout d'un bundle permettant l'enum avec doctrine

--- interface/app/AppKernel.php
+++ interface/app/AppKernel.php
@@ -16,8 +16,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
-					  new FOS\UserBundle\FOSUserBundle(),
+			new FOS\UserBundle\FOSUserBundle(),
             new Vich\UploaderBundle\VichUploaderBundle(),
+            new Fresh\DoctrineEnumBundle\FreshDoctrineEnumBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
