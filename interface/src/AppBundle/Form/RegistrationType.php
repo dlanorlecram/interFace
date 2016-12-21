709c65b1bb0bf9f37978c5f9451ad22e3b296b67
solenehuault <mopi@dotslashplay.it>
2016-12-21 17:27:16 +0100

    src/AppBundle/Form/RegistrationType.php - Suppression du champs username dans le formulaire de création d'utilisateur

--- interface/src/AppBundle/Form/RegistrationType.php
+++ interface/src/AppBundle/Form/RegistrationType.php
@@ -11,6 +11,7 @@ class RegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+		$builder->remove('username');
         $builder->add('nom');
         $builder->add('prenom');
         $builder->add('roles', 'collection', array(
