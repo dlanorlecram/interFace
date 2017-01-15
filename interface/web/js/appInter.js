90f5e951466be9a9e2ed9bb4db0caf1d371406de
dlanorlecram <ronald.marcel.mr@gmail.com>
2017-01-16 00:05:43 +0100

    Début d'intégration du filtre dans la recherche

--- interface/web/js/appInter.js
+++ interface/web/js/appInter.js
@@ -1,27 +1,41 @@
-var app = angular.module("appInter",[]);
+// var app = angular.module("appInter",[]);
+//
+// app.config(function($interpolateProvider){
+//   $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
+// });
 
-app.config(function($interpolateProvider){
+    //app.controller("searchCtrl", function($scope, $log){
 
-  $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
-  
-});
+        //var json=[];
+        // $scope.demandeurTwig = function(data) {
+        //     json = data;
+        //     return json;
+        // };
 
-app.controller("effect", function($scope){
+        //
+        // $scope.sltOptsSort = [{
+        //         type: "select",
+        //         value: "nom",
+        //         name: "Par nom"
+        //     },{
+        //         value: "prenom",
+        //          name: "Par prénom"
+        // }];
+        //
+        // /* Fonction tri */
+        // function trieByID(a,b){
+        //     return a.id - b.id;
+        // }
+        //
+        // $scope.jsonData = json.sort(trieByID);
+        //
+        // console.log($scope.jsonData[0]);
+        //
+        // $log.info($scope.jsonData);
+        // $scope.selecTrie = function( key, value ){
+        //     var obj = {};
+        //     obj[ "" + key.value + "" ] = value;
+        //     return obj;
+        //  };
 
-
-  $scope.statusCheck = function(value){
-    console.log(value);
-    /*if (value = true){
-      value = {
-        'visibility': 'visible'
-      }
-      return value;
-    }
-    else{
-      value = {
-        'visibility': 'hidden'
-      }
-      return value;
-    }*/
-  }
-});
+    //});
